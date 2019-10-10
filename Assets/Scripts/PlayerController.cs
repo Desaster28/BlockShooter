32324ac948f2dc983a0f24e8892f33e3a3296405
Desaster28 <Desaster28@users.noreply.github.com>
2019-10-10 13:41:52 +0200

    Added Everything

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+    private Vector3 _velocity;
+    Rigidbody myRigitBody;
+
+    void Start()
+    {
+        myRigitBody = GetComponent<Rigidbody>();
+    }
+
+    public void Move(Vector3 velocity)
+    {
+        _velocity = velocity;
+    }
+
+    public void LookAt(Vector3 lookPoint)
+    {
+        Vector3 heightCorrectedPoint = new Vector3(lookPoint.x, transform.position.y, lookPoint.z);
+        transform.LookAt(heightCorrectedPoint);
+    }
+
+    public void FixedUpdate()
+    {
+        myRigitBody.MovePosition(myRigitBody.position + _velocity * Time.fixedDeltaTime);
+    }
+}
