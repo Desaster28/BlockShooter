173cf1a03eefb253f8373fdfd0a387fc32678585
Desaster28 <Desaster28@users.noreply.github.com>
2019-10-14 23:40:14 +0200

    Added Gun

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,25 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(PlayerController))]
+[RequireComponent(typeof(GunController))]
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 5;
 
     Camera viewCamera;
     PlayerController controller;
+    GunController gunController;
 
     void Start()
     {
-        controller = GetComponent<PlayerController>();
         viewCamera = Camera.main;
+        controller = GetComponent<PlayerController>();
+        gunController = GetComponent<GunController>();
     }
 
     void Update()
     {
+        //Movement input
         Vector3 moveInput = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
         Vector3 moveVelocity = moveInput.normalized * moveSpeed;
         controller.Move(moveVelocity);
 
+        //Look input
         Ray ray = viewCamera.ScreenPointToRay(Input.mousePosition);
         Plane groundPlane = new Plane(Vector3.up, Vector3.zero);
         float rayDiastance;
@@ -30,6 +35,12 @@ public class Player : MonoBehaviour
         {
             Vector3 point = ray.GetPoint(rayDiastance);
             controller.LookAt(point);
+        }
+
+        //Weapon input
+        if (Input.GetMouseButton(0))
+        {
+            gunController.Shoot();
         }
     }
 }
